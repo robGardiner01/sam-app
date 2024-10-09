dacce71249ac86107c6c47b9958ade146513ee1c
Rob Gardiner <rgardiner@scottlogic.com>
2024-10-09 15:20:06 +0100

    Breaking the lambda function on purpose

--- HelloWorldFunction/src/main/java/helloworld/App.java
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -27,7 +27,7 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
                 .withHeaders(headers);
         try {
             final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            String output = String.format("{ \"message\": \"I'm using canary deployments\", \"location\": \"%s\" }", pageContents);
+            String output = String.format("{ \"message\": \"I'm using canary deployment\", \"location\": \"%s\" }", pageContents);
 
           //  return response
           //          .withStatusCode(200)
