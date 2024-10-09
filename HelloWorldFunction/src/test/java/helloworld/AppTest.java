0f1104894f73028f0e947048efcf78f34af0b0dc
Rob Gardiner <rgardiner@scottlogic.com>
2024-10-09 14:41:30 +0100

    Breaking the lambda function on purpose

--- HelloWorldFunction/src/test/java/helloworld/AppTest.java
+++ HelloWorldFunction/src/test/java/helloworld/AppTest.java
@@ -7,16 +7,16 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class AppTest {
-  @Test
-  public void successfulResponse() {
-    App app = new App();
-    APIGatewayProxyResponseEvent result = app.handleRequest(null, null);
-    assertEquals(500, result.getStatusCode().intValue());
-    assertEquals("application/json", result.getHeaders().get("Content-Type"));
-    String content = result.getBody();
-    assertNotNull(content);
-    assertTrue(content.contains("\"message\""));
-    assertTrue(content.contains("\"I'm using canary deployments\""));
-    assertTrue(content.contains("\"location\""));
-  }
+  // @Test
+  // public void successfulResponse() {
+  //   App app = new App();
+  //   APIGatewayProxyResponseEvent result = app.handleRequest(null, null);
+  //   assertEquals(200, result.getStatusCode().intValue());
+  //   assertEquals("application/json", result.getHeaders().get("Content-Type"));
+  //   String content = result.getBody();
+  //   assertNotNull(content);
+  //   assertTrue(content.contains("\"message\""));
+  //   assertTrue(content.contains("\"I'm using canary deployments\""));
+  //   assertTrue(content.contains("\"location\""));
+  // }
 }
